658e2385bf771d40e0016972c159570f85643f9b
Abhishek Tiwari <tiwariabhishekk10@gmail.com>
2020-03-10 18:44:39 +0530

    Update image_rotation.py

--- Image_rotation_codes/image_rotation.py
+++ Image_rotation_codes/image_rotation.py
@@ -14,9 +14,9 @@ for image_path in os.listdir(path):
     input_path=os.path.join(path,image_path)
     image_to_rotate=cv2.imread(input_path)
     angle=randint(1,359)
-    rotated=imutils.rotate_bound(image_to_rotate,angle)
+    rotated=imutils.rotate_bound(image_to_rotate,angle)#differnce between rotate and rotate_bound is i does not cuts any part of image.
 
     fullpath=os.path.join(outpath,'rotated_'+image_path)
     f.write(image_path+" " +str(angle)+"\n")
-    misc.imsave(fullpath,rotated)
+    cv2.imwrite(fullpath,rotated)
 f.close()
